61fcc4e4d6aa02648033c096604dda6299da700d
Grigory Davydov <gdavidov22@gmail.com>
2024-06-24 07:33:15 -0700

    added selenoid options, chrome tests

--- conftest.py
+++ conftest.py
@@ -1,34 +1,48 @@
 import pytest
 import selenium
 from selenium import webdriver
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.common.by import By
 import re
 
-# PAGE_URL = "https://www.barnesandnoble.com/"
+PAGE_URL = "https://www.barnesandnoble.com/"
 
-# # set up and tear down fixtures for test environments
-# @pytest.fixture
-# def driver():
-#     chrome_options = Options()
-#     # chrome_options.add_argument('--headless')
-#     chrome_options.add_experimental_option(name="detach", value=True)  # Chrome browser will not be closed when the ChromeDriver object is destroyed
-#     chrome_options.add_argument("--start-maximized")
-#     driver = webdriver.Chrome(options=chrome_options)
-#     yield driver  # statement in the fixture function is where the setup part ends and the teardown part begins
-#     driver.close()
-#     print("Browser is closed.")
-#     driver.quit()
-#     print("Driver is quited.")
+# set up and tear down fixtures for test environments
+@pytest.fixture
+def driver():
+    chrome_options = Options()
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_experimental_option(name="detach", value=True)  # Chrome browser will not be closed when the ChromeDriver object is destroyed
+    # chrome_options.add_argument("--start-maximized")
 
-# @pytest.fixture
-# def pass_url():
-#     return PAGE_URL
+    # # LOCAL DRIVER SETUP
+    # driver = webdriver.Chrome(options=chrome_options)
+
+    # REMOTE DRIVER SETUP
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    chrome_options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub", options=chrome_options)
+
+    yield driver  # statement in the fixture function is where the setup part ends and the teardown part begins
+    driver.close()
+    print("Browser is closed.")
+    driver.quit()
+    print("Driver is quited.")
 
 @pytest.fixture
-def before_test():
-    print("\nBefore test")
-    yield
-    print("\nAfter test")
+def pass_url():
+    return PAGE_URL
+
+# @pytest.fixture
+# def before_test():
+#     print("\nBefore test")
+#     yield
+#     print("\nAfter test")
--- tests.py
+++ tests.py
@@ -1,26 +1,25 @@
 import pytest
 import selenium
 from selenium import webdriver
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import sys
 
-# EXPECTED_NUM_OF_BTTNS = 10
+EXPECTED_NUM_OF_BTTNS = 10
 
-# def test_verify_title(driver, pass_url):
-#         driver.get(pass_url)
-#         driver.find_elements(By.XPATH, "//a[text()='Gift Cards']")[0].click()
-#         expected_title = "Barnes & Noble Gift Cards and eGift Cards | Barnes & Noble®"
-#         title = driver.title
-#         assert title == expected_title, f"Expected title: {expected_title} is not matching actual: {title}"
+def test_verify_title(driver, pass_url):
+        driver.get(pass_url)
+        driver.find_elements(By.XPATH, "//a[text()='Gift Cards']")[0].click()
+        expected_title = "Barnes & Noble Gift Cards and eGift Cards | Barnes & Noble®"
+        title = driver.title
+        assert title == expected_title, f"Expected title: {expected_title} is not matching actual: {title}"
 
 
-# def test_verify_num_of_bttns_in_nav_panel(driver, pass_url):
-#         driver.get(pass_url)
-#         nav_panel_bttns = driver.find_elements(By.XPATH, "//div[@id='navbarSupportedContent']//ul[@class='navbar-nav rhf-navbar-nav booksNav mobile-hide']//a[@class='rhf-nav-link nav-link rhf_border_right']")
-#         assert len(nav_panel_bttns) == EXPECTED_NUM_OF_BTTNS, f"Expected number of buttons: {EXPECTED_NUM_OF_BTTNS} is not matching actual: {len(nav_panel_bttns)}"
+def test_verify_num_of_bttns_in_nav_panel(driver, pass_url):
+        driver.get(pass_url)
+        nav_panel_bttns = driver.find_elements(By.XPATH, "//div[@id='navbarSupportedContent']//ul[@class='navbar-nav rhf-navbar-nav booksNav mobile-hide']//a[@class='rhf-nav-link nav-link rhf_border_right']")
+        assert len(nav_panel_bttns) == EXPECTED_NUM_OF_BTTNS, f"Expected number of buttons: {EXPECTED_NUM_OF_BTTNS} is not matching actual: {len(nav_panel_bttns)}"
 
 
 # def just_to_try():
@@ -41,17 +40,17 @@ import sys
 
 # just_to_try()
 
-def test_1():
-    a = 1
-    b = 2
-    assert a == b, f"'{a}' is not equal to '{b}'."
+# def test_1():
+#     a = 1
+#     b = 2
+#     assert a == b, f"'{a}' is not equal to '{b}'."
 
-def test_2():
-    a = 1
-    b = 2
-    assert a != b, f"'{a}' is equal to '{b}'."
+# def test_2():
+#     a = 1
+#     b = 2
+#     assert a != b, f"'{a}' is equal to '{b}'."
 
-def test_3():
-    a = 1
-    b = 2
-    assert a < b, f"'{a}' higher than '{b}'."
+# def test_3():
+#     a = 1
+#     b = 2
+#     assert a < b, f"'{a}' higher than '{b}'."
