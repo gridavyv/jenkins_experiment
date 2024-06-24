36ac2d47a76882a4a29bff39d4805043b8ad5cf1
Grigory Davydov <gdavidov22@gmail.com>
2024-06-24 07:41:14 -0700

    added 1 simple test

--- tests.py
+++ tests.py
@@ -7,19 +7,26 @@ from selenium.webdriver.common.by import By
 import sys
 
 EXPECTED_NUM_OF_BTTNS = 10
+MAIN_PAGE_TITLE = "Online Bookstore: Books, NOOK ebooks, Music, Movies & Toys"
 
-def test_verify_title(driver, pass_url):
+
+def test_verify_main_page_title(driver, pass_url):
         driver.get(pass_url)
-        driver.find_elements(By.XPATH, "//a[text()='Gift Cards']")[0].click()
-        expected_title = "Barnes & Noble Gift Cards and eGift Cards | Barnes & Noble®"
         title = driver.title
-        assert title == expected_title, f"Expected title: {expected_title} is not matching actual: {title}"
+        assert title == MAIN_PAGE_TITLE, f"Expected title: '{MAIN_PAGE_TITLE}' is not matching actual: '{title}'"
+
+# def test_verify_git_page_title(driver, pass_url):
+#         driver.get(pass_url)
+#         driver.find_elements(By.XPATH, "//a[text()='Gift Cards']")[0].click()
+#         expected_title = "Barnes & Noble Gift Cards and eGift Cards | Barnes & Noble®"
+#         title = driver.title
+#         assert title == expected_title, f"Expected title: {expected_title} is not matching actual: {title}"
 
 
-def test_verify_num_of_bttns_in_nav_panel(driver, pass_url):
-        driver.get(pass_url)
-        nav_panel_bttns = driver.find_elements(By.XPATH, "//div[@id='navbarSupportedContent']//ul[@class='navbar-nav rhf-navbar-nav booksNav mobile-hide']//a[@class='rhf-nav-link nav-link rhf_border_right']")
-        assert len(nav_panel_bttns) == EXPECTED_NUM_OF_BTTNS, f"Expected number of buttons: {EXPECTED_NUM_OF_BTTNS} is not matching actual: {len(nav_panel_bttns)}"
+# def test_verify_num_of_bttns_in_nav_panel(driver, pass_url):
+#         driver.get(pass_url)
+#         nav_panel_bttns = driver.find_elements(By.XPATH, "//div[@id='navbarSupportedContent']//ul[@class='navbar-nav rhf-navbar-nav booksNav mobile-hide']//a[@class='rhf-nav-link nav-link rhf_border_right']")
+#         assert len(nav_panel_bttns) == EXPECTED_NUM_OF_BTTNS, f"Expected number of buttons: {EXPECTED_NUM_OF_BTTNS} is not matching actual: {len(nav_panel_bttns)}"
 
 
 # def just_to_try():
